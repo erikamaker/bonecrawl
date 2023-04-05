6598d2bbce96b7ce8356845471b576b103454291
Erika Maker <erikamaker@outlook.com>
2023-04-05 19:18:22 -0400

    Put all presets in one file (getting hard to manage)

--- gameboard.rb
+++ gameboard.rb
@@ -1,11 +1,11 @@
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 #####    GAMEBOARD    ########################################################################################################################################################################################################################################
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 
 
 class Gameboard
     def initialize
-        @@world = [] 
+        @@world = []
         @@check = []
         @@stand = [0,1,2]
         @@items = []
@@ -14,7 +14,6 @@ class Gameboard
         @@souls = 1
         @@pages = 0
         @@stats = {
-            :level => 1,
             :bless => 0,
             :curse => 0,
             :slays => 0,
@@ -22,28 +21,28 @@ class Gameboard
             :score => 0
         }
         @@skill = {
-            :arrows => 0,     
-            :blades => 0,     
-            :magick => 0,     
-            :speech => 0      
+            :arrows => 0,
+            :blades => 0,
+            :magick => 0,
+            :speech => 0
         }
     end
     def action_select
         prompt_player
         process_input
         toggle_interact
-        print "\n\n\n\n" 
-    end	
+        print "\n\n\n\n"
+    end
     def prompt_player
         print Rainbow("\n   What next?").cyan.bright
-        print Rainbow("  >>  ").purple.bright	
+        print Rainbow("  >>  ").purple.bright
     end
-    def process_input	
-        @@action = gets.chomp.downcase								
+    def process_input
+        @@action = gets.chomp.downcase
         sentence = @@action.scan(/[\w']+/)
         @@action = (MOVES.flatten & (sentence)).join('')
-        @@target = (sentence - PARTS).last 
-    end	
+        @@target = (sentence - PARTS).last
+    end
     def reset_input
         @@action = :reset
         @@target = :reset
@@ -51,8 +50,8 @@ class Gameboard
     def toggle_backdrop
         @@state = :backdrop
     end
-    def toggle_interact 
-        moves = MOVES[1..12].flatten 
+    def toggle_interact
+        moves = MOVES[1..12].flatten
         if moves.include?(@@action)
             @@state = :interact
         else toggle_backdrop
@@ -70,18 +69,18 @@ class Gameboard
     def turn_page
         reset_sightline
         increment_page(1)
-        toggle_backdrop  
+        toggle_backdrop
     end
 end
 
 
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 #####    MOVEMENT    #########################################################################################################################################################################################################################################
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 
 
 class Position < Gameboard
-    def directions 
+    def directions
         ["north","south","east","west"]
     end
     def load_directions
@@ -94,10 +93,10 @@ class Position < Gameboard
             directions[2] => [1, 0],
             directions[3] => [-1,0]
         }
-    end 
+    end
     def directed_movement
         @@stand[1] += direction[@@target][0]
-        @@stand[2] += direction[@@target][1] 
+        @@stand[2] += direction[@@target][1]
     end
     def activated_barrier
         @@stand[1] -= direction[@@target][0]
@@ -105,10 +104,10 @@ class Position < Gameboard
     end
     def detect_direction
         if directions.include?(@@target)
-            reposition_player 
-        else 
+            reposition_player
+        else
             no_direction_detected
-        end  
+        end
     end
     def detect_movement
         if MOVES[0].include?(@@action)
@@ -120,15 +119,15 @@ class Position < Gameboard
         directed_movement
         if @@world.include?(@@stand)
             animate_movement
-        else 
+        else
             activated_barrier
             the_way_is_blocked
         end
     end
     def animate_movement
         print "	   - You move #{@@target} to "
-        print Rainbow(	     "[ #{@@stand[1]} , #{@@stand[2]} ]").orange	
-        print ".\n\n"	
+        print Rainbow(	     "[ #{@@stand[1]} , #{@@stand[2]} ]").orange
+        print ".\n\n"
     end
     def the_way_is_blocked
         puts "	   - The #{@@target}ern way is blocked."
@@ -146,13 +145,13 @@ class Position < Gameboard
         print "	     "
         print_movement_options
         puts ".\n\n"
-    end									 
+    end
 end
 
 
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 #####    INTERFACE    ########################################################################################################################################################################################################################################
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 
 
 class Interface < Gameboard
@@ -162,7 +161,7 @@ class Interface < Gameboard
         print_spirit_meter
         print Rainbow(" -   ╱   - ").blue.bright
 		print_hearts_meter
-		print Rainbow(" -     ]").blue.bright 		
+		print Rainbow(" -     ]").blue.bright
 		print Rainbow("\n---------------------------------------------------------\n\n\n").blue.bright
     end
     def print_spirit_meter
@@ -176,7 +175,7 @@ class Interface < Gameboard
 		@@heart = [@@heart, 4].min
 		if @@heart > 1
 		@@heart.times { print Rainbow("♥ ").red }
-		else 
+		else
             print Rainbow("♥ ").red.blink
 		end
 		(4 - @@heart).times { print Rainbow("♥ ").cyan }
@@ -190,9 +189,9 @@ class Interface < Gameboard
 	def tutorial
 		if that_move_made_no_sense
 			return if player_selected_help_menu
-			toggle_backdrop	
+			toggle_backdrop
 			print "	   - You pause for one page. View\n"
-			print "	     tutorial with command" 
+			print "	     tutorial with command"
             print Rainbow(" help").cyan + ".\n\n"
             decrement_page(1)
 		end
@@ -206,10 +205,10 @@ class Interface < Gameboard
         puts "	     your knapsack.\n\n"
     end
 	def help_menu
-		if MOVES[13].include?(@@action)																																
-			@@pages -= 1																	
-			puts "	   - Speak your move plainly in a"  													
-			puts "	     few short words, referencing"														
+		if MOVES[13].include?(@@action)
+			@@pages -= 1
+			puts "	   - Speak your move plainly in a"
+			puts "	     few short words, referencing"
 			puts "	     just one subject per page.\n\n"
 			puts Rainbow("	     Check the area.").blue
 			puts Rainbow("	     Talk to the wizard.").indigo
@@ -219,17 +218,17 @@ class Interface < Gameboard
 			puts Rainbow("	     Eat some bread.").green
 			puts Rainbow("	     Walk south.").cyan
 			puts Rainbow("	     Unlock the door.").blue
-			puts Rainbow("	     Check my stats.\n").indigo											
+			puts Rainbow("	     Check my stats.\n").indigo
 			puts "	   - Pause to view your current"
-			print "	     position by pressing " 
+			print "	     position by pressing "
 			print Rainbow("return").cyan + ".\n\n"
 		end
     end
-	def page_bottom	
+	def page_bottom
 		puts "\n\n\n"
-        z = @@stand[0]																				
-		x = @@stand[1]																				
-		y = @@stand[2]	
+        z = @@stand[0]
+		x = @@stand[1]
+		y = @@stand[2]
 		bigmap = [
 			[[z, x - 1, y + 1], [z, x, y + 1], [z, x + 1, y + 1]],
 			[[z, x - 1, y], [z, x, y], [z, x + 1, y]],
@@ -242,14 +241,14 @@ class Interface < Gameboard
 					print Rainbow("■ ").red.blink
 				elsif @@world.include?(pos)
 					print Rainbow("■ ").green
-				else 
+				else
                     print "⬚ "
 				end
 			end
 			print "\n" if row != bigmap.last
-		end	
+		end
         (37 - @@pages.to_s.length).times { print(" ") }
-		print Rainbow("- Pg. #{@@pages} -\n\n").purple									
-	end	
+		print Rainbow("- Pg. #{@@pages} -\n\n").purple
+	end
 end
 
--- main.rb
+++ main.rb
@@ -1,141 +1,132 @@
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 #####    DEPENDENCIES    #####################################################################################################################################################################################################################################
-############################################################################################################################################################################################################################################################## 
-	
+##############################################################################################################################################################################################################################################################
+
+
 require 'rainbow'
-require './vocabulary.rb' 
-require './gameboard.rb'         
+require './vocabulary.rb'
+require './gameboard.rb'
 require './gamepiece.rb'
-require './assets/fixtures.rb'
-require './assets/materials.rb'
-require './assets/tools.rb'
-require './assets/weapons.rb'
-require './assets/wearables.rb'
-require './assets/edibles.rb'
-require './assets/containers.rb'
-require './assets/pullables.rb'
-require './assets/trees.rb'
-require './assets/fruit.rb'
+require './game_pieces.rb'
 
 
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 #####    GAME START    #######################################################################################################################################################################################################################################
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 
 
-print "\e[8;57;57t"	
+print "\e[8;57;57t"
 
 console = Interface.new
 player = Gameboard.new
 location = Position.new
 rucksack = Inventory.new
-stats = StatsCard.new
 
 
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 #####    LEVEL 1    ##########################################################################################################################################################################################################################################
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 
 
-room_1 = Dungeon.new                                          
-room_1.minimap = [[0,1,1],[0,1,2],[0,2,1],[0,2,2]]            
-def room_1.overview                                                 
-    puts "	   - By the glow of one northeast"          
-    puts "	     torch, you glance around. A"            
-    puts "	     crude stone toilet hangs out"                   
-    puts "	     of the southwest corner near"                  
-    puts "	     your cell's only exit.\n\n"                    
-end                                                         
+room_1 = Dungeon.new
+room_1.minimap = [[0,1,1],[0,1,2],[0,2,1],[0,2,2]]
+def room_1.overview
+    puts "	   - By the glow of one northeast"
+    puts "	     torch, you glance around. A"
+    puts "	     crude stone toilet hangs out"
+    puts "	     of the southwest corner near"
+    puts "	     your cell's only exit.\n\n"
+end
 
-torch_1 = Torch.new                                           
-torch_1.minimap = [[0,2,2],[0,2,-1],[0,5,-4]]                
+torch_1 = Torch.new
+torch_1.minimap = [[0,2,2],[0,2,-1],[0,5,-4]]
 
-drain_1 = Toilet.new                                        
-drain_1.minimap = [[0,1,1]]                                 
-drain_1.content = Lockpick.new                             
+drain_1 = Toilet.new
+drain_1.minimap = [[0,1,1]]
+drain_1.content = Lockpick.new
 
-hall_1 = Corridor.new                                        
-hall_1.minimap = [[0,2,0],[0,2,-1],[0,2,-2]]                  
-def hall_1.overview                                           
-    puts "	   - It's a narrow corridor south"                
-    puts "	     of your cell. A faint clang"                 
-    puts "	     of metal striking metal can"                
+hall_1 = Corridor.new
+hall_1.minimap = [[0,2,0],[0,2,-1],[0,2,-2]]
+def hall_1.overview
+    puts "	   - It's a narrow corridor south"
+    puts "	     of your cell. A faint clang"
+    puts "	     of metal striking metal can"
     puts "	     be heard to the southeast.\n\n"
 end
 
-door_1 = Door.new                                             
-door_1.minimap = [[0,2,1]]                                    
+door_1 = Door.new
+door_1.minimap = [[0,2,1]]
 door_1.content = hall_1
 
-hook_1 = Hook.new                                                       
-hook_1.minimap = [[0,2,0]]                                    
+hook_1 = Hook.new
+hook_1.minimap = [[0,2,0]]
 
-key_1 = Key.new                                               
-key_1.minimap = [[0,2,0]]                                      
-def key_1.backdrop                                                    
-    puts "	   - A brass key dangles from it.\n\n"                                     
+key_1 = Key.new
+key_1.minimap = [[0,2,0]]
+def key_1.backdrop
+    puts "	   - A brass key dangles from it.\n\n"
 end
 
 room_2 = Dungeon.new
 room_2.minimap = [[0,1,-3],[0,2,-3],[0,3,-3],[0,1,-4],[0,2,-4],[0,3,-4],[0,1,-5],[0,2,-5],[0,3,-5]]
-def room_2.backdrop                                           
-    puts "	   - You're in a homely guardroom."                   
-    puts "	     It's warm, well-lit, and dry.\n\n"           
+def room_2.backdrop
+    puts "	   - You're in a homely guardroom."
+    puts "	     It's warm, well-lit, and dry.\n\n"
 end
-def room_2.overview                                           
-    puts "	   - It's a cozy supply room where"               
-    puts "	     goblin guards eat and prepare"               
+def room_2.overview
+    puts "	   - It's a cozy supply room where"
+    puts "	     goblin guards eat and prepare"
     puts "	     to hunt. A warm fire roars in"
     puts "	     the western wall. A big table"
     puts "	     is set in the center.\n\n"
 end
 
-chest_1 = Chest.new                                           
-chest_1.minimap = [[0,5,-4]]                                  
-chest_1.content = Knife3.new                                  
+chest_1 = Chest.new
+chest_1.minimap = [[0,5,-4]]
+chest_1.content = Knife3.new
 
-pull_1 = Lever.new                                            
-pull_1.minimap = [[0,3,-3]]                                   
-pull_1.content = chest_1                                      
-def pull_1.reveal_secret													
-    puts Rainbow("	   - Something heavy crashes east").orange	
-    print Rainbow("	     of the warm supply room.").orange    
-    print " For\n"			
-    puts "	     a moment, your ears ring and"                
-    puts "	     your heart races.\n\n"                       
-    puts "	   - But, if it really had been a"             
+pull_1 = Lever.new
+pull_1.minimap = [[0,3,-3]]
+pull_1.content = chest_1
+def pull_1.reveal_secret
+    puts Rainbow("	   - Something heavy crashes east").orange
+    print Rainbow("	     of the warm supply room.").orange
+    print " For\n"
+    puts "	     a moment, your ears ring and"
+    puts "	     your heart races.\n\n"
+    puts "	   - But, if it really had been a"
     puts "	     goblin, you would already be"
-    puts "	     hung on that rusty hook back" 
+    puts "	     hung on that rusty hook back"
     puts "	     outside your cell door.\n\n"
-end 
+end
 
-door_2 = Door.new                                             
+door_2 = Door.new
 door_2.minimap = [[0,2,-2]]
 door_2.content = room_2
 
-table_1 = Table.new                                         
+table_1 = Table.new
 table_1.minimap = [[0,2,-4]]
 
-food_1 = Apple.new                                            
-food_1.minimap = [[0,2,-4]]                               
+food_1 = Apple.new
+food_1.minimap = [[0,2,-4]]
 
-hall_2 = Corridor.new                                         
+hall_2 = Corridor.new
 hall_2.minimap = [[0,4,-4],[0,5,-4],[0,6,-4]]
-def hall_2.overview                                           
-    puts "	   - It's a narrow hallway east of" 
+def hall_2.overview
+    puts "	   - It's a narrow hallway east of"
     puts "	     of the goblin supply chamber."
     puts "	     The clanging grows louder.\n\n"
 end
 
-door_3 = Door.new                                            
+door_3 = Door.new
 door_3.minimap = [[0,3,-4]]
 door_3.content = hall_2
 
-pick_1 = Lockpick.new                                         
+pick_1 = Lockpick.new
 pick_1.minimap = [[0,4,-4]]
 
-drug_1 = RedFlower.new                                         
-drug_1.minimap = [[0,2,1]]                                    
+drug_1 = RedFlower.new
+drug_1.minimap = [[0,2,1]]
 
 
 lighter = Lighter.new
@@ -153,9 +144,9 @@ apples.minimap = [[0,6,-4]]
 level_1 = [ room_1, drain_1, lighter, grease, door_1, hook_1, key_1, door_2, door_3, tree, apples, torch_1, table_1, food_1, pull_1, pick_1, drug_1 ]
 
 
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 #####    GAME LOOP     #######################################################################################################################################################################################################################################
-############################################################################################################################################################################################################################################################## 
+##############################################################################################################################################################################################################################################################
 
 
 loop do
@@ -163,12 +154,12 @@ loop do
     console.page_top
     player.action_select
     console.help_menu
-    console.tutorial  
-    location.detect_movement     
+    console.tutorial
+    location.detect_movement
     level_1.each { |piece| piece.assemble }
     rucksack.assemble
     stats.assemble
-    console.no_target		
+    console.no_target
     console.page_bottom
 end
 
